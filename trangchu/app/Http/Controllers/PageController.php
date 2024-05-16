903e97fd0337909dd373e74b92445638a6836d08
NgThanhHung04 <nguyenthanhhungg04@gmail.com>
2024-05-17 01:19:42 +0700

    Update message

--- trangchu/app/Http/Controllers/PageController.php
+++ trangchu/app/Http/Controllers/PageController.php
@@ -21,8 +21,14 @@ class PageController extends Controller
     public function getLoaiSp(){
         return view('page.loai_sanpham');
     }
-    public function getChitiet(){
-        return view('page.chitiet_sanpham');
+    public function getChitiet(Request $req){
+        $sanpham = Product::where('id', $req->id)->first();
+        if ($sanpham === null) {
+            // Xử lý khi không tìm thấy sản phẩm, ví dụ: hiển thị thông báo lỗi hoặc chuyển hướng người dùng đến trang khác
+            abort(404);
+        }
+        
+        return view('page.chitiet_sanpham', compact('sanpham'));
     }
 
     public function getLienhe(){
