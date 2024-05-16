0ce6e1f15fb76101b120b644adb6341460cbdb4d
NgThanhHung04 <nguyenthanhhungg04@gmail.com>
2024-05-17 01:28:26 +0700

    Thanh Hùngg up trang chitietsanpham

---
+++ chitietsanpham/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+ use Illuminate\Http\Request;
+
+use App\Http\Controllers\PageController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('index', 'App\Http\Controllers\PageController@getIndex')->name('trang-chu');
+
+
+Route::get('chi-tiet-san-pham', 'App\Http\Controllers\PageController@getChitiet')->name('chitietsanpham');
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
